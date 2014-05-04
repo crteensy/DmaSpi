e6d82591d17c65a45468c4b39dab78cb4c6cb216
crteensy <crteensy@users.noreply.github.com>
2014-05-05 00:12:06 +0200

    Create DmaSpi.cpp

---
+++ DmaSpi.cpp
@@ -0,0 +1,14 @@
+#include "DmaSpi.h"
+
+DmaSpi0 DMASPI0;
+
+void dma_ch0_isr()
+{
+  DmaSpi0::m_rxIsr();
+}
+
+DmaSpi0::Transfer* volatile DmaSpi0::m_pCurrentTransfer = nullptr;
+DmaSpi0::Transfer* volatile DmaSpi0::m_pNextTransfer = nullptr;
+DmaSpi0::Transfer* volatile DmaSpi0::m_pLastTransfer = nullptr;
+volatile uint32_t DmaSpi0::m_ctarBackup;
+volatile uint8_t DmaSpi0::m_devNull;
