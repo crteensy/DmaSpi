4df87e5816589ad53f3edb45d79193ab586dcdfc
crteensy <crteensy@users.noreply.github.com>
2014-05-05 00:14:13 +0200

    Create DMASpi_example1.ino

---
+++ examples/DMASpi_example1/DMASpi_example1.ino
@@ -0,0 +1,24 @@
+#include <ChipSelect.h>
+#include <DmaSpi.h>
+
+// create a chip select object. This one uses the pin that the built-in LED is connected to.
+ActiveLowChipSelect<LED_BUILTIN> cs;
+
+void setup() {
+  DMASPI0.begin();
+  // create a transfer object
+  DmaSpi0::Transfer trx(nullptr, 100, nullptr, 0xFF, &cs);
+
+  // and register it. If the DMA SPI is idle, it will immediately start transmitting. Otherwise the transfer is added to a queue.
+  DMASPI0.registerTransfer(trx);
+
+  /** FREE CPU TIME YOU DIDN'T HAVE BEFORE! DO SOMETHING USEFUL! **/
+
+  // wait for the transfer to finish
+  while(trx.busy());
+}
+
+void loop() {
+
+}
+
