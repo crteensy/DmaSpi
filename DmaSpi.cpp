da9eb669069ccc5b0ba16575ceaa5dc0357c74b4
crteensy <crteensy@users.noreply.github.com>
2014-05-11 00:57:35 +0200

    Update DmaSpi.cpp
    added DmaSpi0::pause_ member

--- DmaSpi.cpp
+++ DmaSpi.cpp
@@ -12,3 +12,4 @@ DmaSpi0::Transfer* volatile DmaSpi0::m_pNextTransfer = nullptr;
 DmaSpi0::Transfer* volatile DmaSpi0::m_pLastTransfer = nullptr;
 volatile uint32_t DmaSpi0::m_ctarBackup;
 volatile uint8_t DmaSpi0::m_devNull;
+bool DmaSpi0::pause_ = false;
